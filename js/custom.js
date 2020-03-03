8d5cb7000f41001afbad45cf17166035e7c31225
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2020-03-03 11:23:12 -0800

    top-bar section done with responsive

---
+++ js/custom.js
@@ -0,0 +1,88 @@
+function openNav() {
+  document.getElementById("mySidenav").style.right = "0";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.right = "-100%";
+}
+
+// slider-carousel
+$('.slider-carousel').slick({
+  dots: false,
+  prevArrow: false,
+  nextArrow: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+// digital-carousel
+$('.digital-carousel').slick({
+  dots: false,
+  prevArrow: false,
+  nextArrow: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});

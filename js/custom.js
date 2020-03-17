1f0498833191afe56cb07292ae2b07b324054c69
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2020-03-17 09:08:45 -0700

    featured section done without responsive

--- js/custom.js
+++ js/custom.js
@@ -6,6 +6,67 @@ function closeNav() {
   document.getElementById("mySidenav").style.right = "-100%";
 }
 
+// digital-carousel
+$('.digital-carousel').slick({
+  dots: false,
+  prevArrow: true,
+  nextArrow: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+        arrows: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+// read-more / read-less
+function myFunction() 
+{
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
 // slider-carousel
 $('.slider-carousel').slick({
   dots: false,
@@ -32,46 +93,6 @@ $('.slider-carousel').slick({
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
-
-// digital-carousel
-$('.digital-carousel').slick({
-  dots: false,
-  prevArrow: false,
-  nextArrow: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
-        arrows: false
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
       }
     },
     {
